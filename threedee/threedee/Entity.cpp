cdd5f2833fa3bd071db11fa48294b075acb04126
Charlie Jenkinson <charliejenko1@outlook.com>
2019-03-11 00:38:57 +0000

    added comments to files that needed them.

--- threedee/threedee/Entity.cpp
+++ threedee/threedee/Entity.cpp
@@ -4,17 +4,17 @@
 
 Entity::Entity(sf::Vector2f pos, sf::Vector2f size)
 {
-	shape.setPosition(pos);
-	shape.setSize(size);
-	shape.setFillColor(sf::Color::Yellow);
+	shape.setPosition(pos); // Set the position of the shape.
+	shape.setSize(size); // Set the size of the shape.
+	shape.setFillColor(sf::Color::Yellow); // Set the colour of the shape.
 }
 
 void Entity::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
-	target.draw(shape);
+	target.draw(shape); // Draw the entity shape.
 }
 
 sf::RectangleShape Entity::GetShape()
 {
-	return shape;
+	return shape; // Return the entity shape.
 }
